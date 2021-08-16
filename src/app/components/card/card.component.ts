ca442a39b45677d9cc556e8b86d6f52c7f5eafde
Nazareno Gabriel Bastanzo <b.nazarenogabriel@gmail.com>
2021-08-16 16:07:47 -0300

    Home Component

--- src/app/components/card/card.component.ts
+++ src/app/components/card/card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-card',
@@ -7,9 +7,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CardComponent implements OnInit {
 
+  @Input() dataEntrante: any;
+
   constructor() { }
 
   ngOnInit(): void {
+    console.log(this.dataEntrante);
+
   }
 
 }
--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { RequestsService } from '../../services/requests.service';
 
 @Component({
   selector: 'app-home',
@@ -7,7 +8,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  goodTeam: any = [];
+  badTeam: any = [];
 
-  ngOnInit(): void { }
+  constructor(
+    private reqServ: RequestsService
+  ) { }
+
+  ngOnInit(): void {
+    for (let i = 1; i <= 731; i++) {
+      if (i <= 6) {
+        this.getHero(i);
+      } else {
+        break;
+      }
+    }
+  }
+
+  getHero(id: number): any {
+    this.reqServ.getHero(id)
+      .then((hero: any) => {
+        console.log(hero);
+        if (hero.data.biography.alignment === 'good') this.goodTeam.push(hero);
+        if (hero.data.biography.alignment === 'bad') this.badTeam.push(hero);
+      })
+      .catch((error: any) => {
+        console.log(error);
+      });
+  }
 }
